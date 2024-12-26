9c1fc6198750c04835feb6b872bd8c5669871f23
Laimeiyunzn <46795462+Laimeiyunzn@users.noreply.github.com>
2024-12-27 06:34:49 +0800

    更新 Eight-word mantra.js

--- Js/Eight-word mantra.js
+++ Js/Eight-word mantra.js
@@ -1,10 +1,11 @@
 [rewrite_local]
 
-^https:\/\/bzpp2\.iwzbz\.com\/api\/v1\.1\/user\/getvipinfo url script-response-body https://raw.githubusercontent.com/Laimeiyunzn/Js/Eight-word mantra.js
+^http[s]?:\/\/bzpp2.iwzbz.com/api/v1.1/user/getvipinfo url script-response-body https://raw.githubusercontent.com/Laimeiyunzn/Js/Eight-word mantra.js
 [mitm] 
 
 hostname = bzpp2.iwzbz.com
 
 
-var ojbk={"code":-1,"data":{"vipTipsType":0,"vipLevel":3,"adtype":0,"expires":"2099-12-31 23:59:59"},"message":"","validateKey":"","isSuccess":true};
-$done({body:JSON.stringify(ojbk)});
+var objc = JSON.parse($response.body);
+    objc = {"code":-1,"data":{"vipTipsType":0,"vipLevel":3,"adtype":0,"expires":"2099-12-31 23:59:59"},"message":"","validateKey":"","isSuccess":true};
+$done({body : JSON.stringify(objc)});
